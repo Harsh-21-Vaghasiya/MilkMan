d1f9fdd8ce9af43564baae353d392d1a4b2823d9
Harsh <harshvaghasiya21@gmail.com>
2023-04-30 21:03:36 +0530

    database sql file added

---
+++ milkmanandroid/app/src/main/java/com/example/milkman/Otp_Validation.java
@@ -0,0 +1,14 @@
+package com.example.milkman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Otp_Validation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_validation);
+    }
+}

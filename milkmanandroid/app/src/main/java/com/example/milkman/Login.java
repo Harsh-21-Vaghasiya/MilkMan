d95b4417d4c44cd4cedbb80d7ab35bceb6245522
Harsh <harshvaghasiya21@gmail.com>
2023-05-01 11:40:54 +0530

    filenot found error solved

--- milkmanandroid/app/src/main/java/com/example/milkman/Login.java
+++ milkmanandroid/app/src/main/java/com/example/milkman/Login.java
@@ -64,7 +64,7 @@ public class Login extends AppCompatActivity {
                             String[] data = new String[2];
                             data[0] = username;
                             data[1] = password;
-                            PutData putData = new PutData("http://192.168.111.67/milkman/milkmanserver/login.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.43.244/milkman/milkmanserver/login.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
 

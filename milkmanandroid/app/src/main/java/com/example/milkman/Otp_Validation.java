26fca0e458e79adf5b23aaff541996e9bfb445c9
Harsh <harshvaghasiya21@gmail.com>
2023-05-03 18:58:55 +0530

    opt page updated

--- milkmanandroid/app/src/main/java/com/example/milkman/Otp_Validation.java
+++ milkmanandroid/app/src/main/java/com/example/milkman/Otp_Validation.java
@@ -1,6 +1,7 @@
 package com.example.milkman;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,22 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.chaos.view.PinView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Otp_Validation extends AppCompatActivity {
     TextInputLayout Enter_Your_Opt_Text_Box;
-    TextInputEditText Otp;
-    Button buttonOtp;
+//    TextInputEditText Otp;
+//    Button buttonOtp;
+    AppCompatButton buttonOtp;
+    PinView Otp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_validation);
 
 
-        Enter_Your_Opt_Text_Box = findViewById(R.id.Enter_Your_Opt_Text_Box);
-        Otp = findViewById(R.id.Otp);
+//        Enter_Your_Opt_Text_Box = findViewById(R.id.Enter_Your_Opt_Text_Box);
+        Otp = findViewById(R.id.pin_view);
         buttonOtp = findViewById(R.id.buttonOtp);
 
 
@@ -51,7 +55,7 @@ public class Otp_Validation extends AppCompatActivity {
                             //Creating array for data
                             String[] data = new String[1];
                             data[0] = androidOtp;
-                            PutData putData = new PutData("http://192.168.43.244/milkman/milkmanserver/otp_authentication.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.10.67/milkman/milkmanserver/otp_authentication.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
 

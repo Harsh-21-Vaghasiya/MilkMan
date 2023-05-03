bcf8d223f9d77c09765509a9e3234224ac59a96f
Harsh <harshvaghasiya21@gmail.com>
2023-05-03 21:58:14 +0530

    mobile no authentication done with otp validation

---
+++ milkmanandroid/app/src/main/java/com/example/milkman/mobileNo_auth.java
@@ -0,0 +1,14 @@
+package com.example.milkman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mobileNo_auth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_no_auth);
+    }
+}

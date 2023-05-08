93871c932f3d265e12add4af1450943d3388331f
Harsh <harshvaghasiya21@gmail.com>
2023-05-08 22:37:29 +0530

    new files added

---
+++ milkmanandroid/app/src/main/java/com/example/milkman/customer_notification.java
@@ -0,0 +1,14 @@
+package com.example.milkman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customer_notification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_notification);
+    }
+}
---
+++ milkmanandroid/app/src/main/java/com/example/milkman/provider_profile.java
@@ -0,0 +1,14 @@
+package com.example.milkman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class provider_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_profile);
+    }
+}
